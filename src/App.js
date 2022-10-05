c13d372684aaf5413fb8e982420d6a6e2b58fffd
web-vikas <mag_vikas@yahoo.com>
2022-10-05 10:35:04 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route exact path='/buffee' element={<Home />} />
         <Route exact path='/' element={<Home />} />
         <Route path='search/*' element={<Search />} />
         <Route path='download/*' element={<DownloadFile />} />
--- src/pages/components/DowbloadBtn/Downloadbtn.jsx
+++ src/pages/components/DowbloadBtn/Downloadbtn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Downloadbtn = (prpos) => {
   return (
     <h2>
-      <a href={prpos.link} target="_self" download>Download {prpos.res}</a>
+      <a href={prpos.link} target="_blank" download>Download {prpos.res}</a>
     </h2>
   )
 }
